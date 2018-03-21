5793a80d6d5e1f598100cbe55fd069a8ea7eee9b
chenqiang728 <358698895@qq.com>
2018-03-21 09:07:03 +0800

    陈强5.0 2018-03-21 apply的service

--- src/main/java/cn/yingchuang/dao/Apply/ApplyMapper.java
+++ src/main/java/cn/yingchuang/dao/Apply/ApplyMapper.java
@@ -9,9 +9,13 @@ import java.util.List;
  */
 public interface ApplyMapper {
 
-    //添加
-    public int addApply(Apply apply);
+    //根据身份证的信息查询一条记录，在页面的时候搜索使用的，需要使用报名表和信息表和比赛表关联
+    public Apply queryApplyByIdNumber(String idNumber);
 
-    //查询所有
+    //查询所有的报名信息  给管理员使用的需要关联information表和race表和apply表
     public List<Apply> queryAllApply();
+
+    //根据raceId查多个报名者的信息
+    public List<Apply> queryApplyByRaceId(Integer raceId);
+
 }
