902ce104082be233e0f584ff70830d02a1646e9c
chenqiang728 <358698895@qq.com>
2018-03-23 11:54:11 +0800

    陈强  2018-3-23  加了点支付的

--- src/main/java/cn/yingchuang/command/config/AlipayConfig.java
+++ src/main/java/cn/yingchuang/command/config/AlipayConfig.java
@@ -15,7 +15,7 @@ public class AlipayConfig {
     // 请求网关地址
     public static String URL = "https://openapi.alipaydev.com/gateway.do";
     // 编码
-    public static String CHARSET = "UTF-8";
+    public static String CHARSET = "gbk";
     // 返回格式
     public static String FORMAT = "json";
     // 支付宝公钥
--- src/main/java/cn/yingchuang/entity/Members.java
+++ src/main/java/cn/yingchuang/entity/Members.java
@@ -11,7 +11,8 @@ public class Members {
     private Information information;
     private String memberCode;
 
-
+    public Members() {
+    }
 
     public Members(int id, String userName, String password, String nickName, Information information, String memberCode) {
         this.id = id;
