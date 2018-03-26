e23e27eb5395e34ab052f925bee2b457e48b56a1
msi <1635941948@qq.com>
2018-03-26 09:02:59 +0800

    整合

--- src/main/java/cn/yingchuang/controller/apply/applyController.java
+++ src/main/java/cn/yingchuang/controller/apply/applyController.java
@@ -1,8 +1,6 @@
 package cn.yingchuang.controller.apply;
 
 import cn.yingchuang.entity.Apply;
-import cn.yingchuang.entity.Information;
-import cn.yingchuang.entity.Members;
 import cn.yingchuang.entity.Race;
 import cn.yingchuang.service.apply.ApplyService;
 import cn.yingchuang.service.race.RaceService;
@@ -15,16 +13,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
- * Created by 祖龙浩 on 2018/3/20.
+ * Created by Administrator on 2018/3/21 0021.
  */
 @Controller
 @RequestMapping("apply")
-public class applyController {
-
+public class ApplyController {
     @Resource
     private ApplyService applyService;
     @Resource
@@ -56,43 +52,4 @@ public class applyController {
         Apply apply = applyService.queryApplyById(applyId);
         return JSON.toJSONString(apply);
     }
-
-    @ResponseBody
-    @RequestMapping(value = "addApplyByMember",method = RequestMethod.POST,produces = {"application/json;charset=utf-8"})
-    public String addApplyByMember(Integer raceId, HttpSession session){
-        Integer informationId = ((Members)session.getAttribute("loginUser")).getInformation().getId();
-        Apply apply = applyService.queryApplyByRaceIdAndInformationId(raceId, informationId);
-        if(apply != null){
-            return JSON.toJSONString("该用户已经报名,无法重复报名");
-        }
-        Integer rows = applyService.addApplyByMember(raceId, informationId);
-        if(rows > 0){
-            return JSON.toJSONString("报名成功");
-        }else{
-            return JSON.toJSONString("报名失败");
-        }
-
-    }
-    @ResponseBody
-    @RequestMapping(value = "addApplyByNoMember",method = RequestMethod.POST,produces = {"application/json;charset=utf-8"})
-    public String addApplyByNoMember(Integer raceId, String playerName,Integer sex,String phoneNumber,String Email,String sick,String dangerName,String idNumber,String dangerPhone){
-        Information information = new Information();
-        information.setPlayerName(playerName);
-        information.setPhoneNumber(phoneNumber);
-        information.setDangerPhone(dangerPhone);
-        information.setSex(sex);
-        information.setEmail(Email);
-        information.setSick(sick);
-        information.setDangerName(dangerName);
-        information.setIdNumber(idNumber);
-
-        System.out.println(information);
-        Integer rows = applyService.addApplyByNoMember(raceId, information);
-        if(rows > 0){
-            return JSON.toJSONString("报名成功");
-        }else{
-            return JSON.toJSONString("报名失败");
-        }
-    }
-
 }
